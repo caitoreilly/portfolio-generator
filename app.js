322bdd5aced554d23beb5ab929b9bf9ca5afd6b8
Caitlin O'Reilly <coreilly29@gmail.com>
2022-09-24 13:47:39 -0400

    added validation property to question prompts

--- app.js
+++ app.js
@@ -15,12 +15,28 @@ const promptUser = () => {
     {
       type: "input",
       name: "name",
-      message: "What is your name?",
+      message: "What is your name? (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name!");
+          return false;
+        }
+      },
     },
     {
       type: "input",
       name: "github",
-      message: "Enter your GitHub username",
+      message: "Enter your GitHub username (Required)",
+      validate: (githubNameInput) => {
+        if (githubNameInput) {
+          return true;
+        } else {
+          console.log("Please enter your GitHub username!");
+          return false;
+        }
+      },
     },
     {
       type: "input",
@@ -47,12 +63,28 @@ Add a New Project
       {
         type: "input",
         name: "name",
-        message: "What is the name of your project?",
+        message: "What is the name of your project? (Required)",
+        validate: (projectNameInput) => {
+          if (projectNameInput) {
+            return true;
+          } else {
+            console.log("You need to enter the name of your project!");
+            return false;
+          }
+        },
       },
       {
         type: "input",
         name: "description",
         message: "Provide a description of the project (Required)",
+        validate: (descriptionInput) => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log("You need to enter a project description!");
+            return false;
+          }
+        },
       },
       {
         type: "checkbox",
@@ -72,6 +104,14 @@ Add a New Project
         type: "input",
         name: "link",
         message: "Enter the GitHub link to your project. (Required)",
+        validate: (linkInput) => {
+          if (linkInput) {
+            return true;
+          } else {
+            console.log("You need to enter the GitHub link!");
+            return false;
+          }
+        },
       },
       {
         type: "confirm",
